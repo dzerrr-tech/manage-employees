1c65b06b5d75fbc0b4812731de94c06aeafe2e4e
dzerrr-tech <hutreon@gmail.com>
2026-08-30 01:55:57 +0700

    .
    .

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { initializeApp, cert } = require('firebase-admin/app');
@@ -28,19 +27,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const frontendPath = path.join(__dirname, '..', 'frontend');
-app.use(express.static(frontendPath));
-
+app.get('/', (req, res) => {
+  res.json({ message: 'Server jalan!' });
+});
 
 app.use('/api/karyawan', require('./routes/karyawan')(db));
 app.use('/api/absensi', require('./routes/absensi')(db));
 app.use('/api/cuti', require('./routes/cuti')(db));
-
-
-app.get('/api/health', (req, res) => {
-  res.json({ message: 'Server jalan!' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
